ac6b4598aaa41a001914acfc25ffc2495d455879
andrescn2020 <andrescn_2010@hotmail.com>
2022-07-04 14:46:51 -0300

    Deplo 2 PORT

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { preCharge } = require('./src/controllers/publicationControl.js');
+const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: false }).then(() => {
 
   preCharge();
 
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+  server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
   });
 
 });
